d62944d0b25358c77827c48a1a5e2299edaf9098
Gustavo Ferreira <70302908+Gussstavof@users.noreply.github.com>
2023-07-18 21:04:33 -0300

    add header and footer

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function Header(){
+    return(
+        <header>
+            <nav className="text-center">
+                <div className="title">Banco</div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
